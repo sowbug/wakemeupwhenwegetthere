48b85e9bb750d19a9cd5b7ec1d04bb931f72e90b
Mike Tsao <mike@sowbug.com>
2014-02-12 01:37:11 +0000

    Full-featured, fakest proof of concept ever created.

--- www/index.js
+++ www/index.js
@@ -1,9 +1,26 @@
+var driverExtensionId = "lcnailakdjhccbcepicgnekpemejahli";
+var port;
+
 function initialize() {
   // Create the autocomplete object, restricting the search
   // to geographical location types.
   autocomplete = new google.maps.places.Autocomplete(
     (document.getElementById('location-input')),
     { types: ['geocode'] });
+
+  port = chrome.runtime.connect(driverExtensionId);
+  if (port) {
+    console.log("established port", port, driverExtensionId);
+    port.onMessage.addListener(function(message) {
+      if (message.location) {
+        console.log("we are here!", message.location);
+        $("#arrival-location").text(message.location);
+        $("#arrived-modal").modal();
+      }
+    });
+  } else {
+    console.log("did not establish port");
+  }
 }
 
 // [START region_geolocation]
@@ -21,7 +38,20 @@ function geolocate() {
 }
 // [END region_geolocation]
 
+function setLocation() {
+  var location = $("#location-input").val();
+  var message = {
+    'location': location,
+    'url': window.location.href  // that's not confusing at all
+  };
+
+  if (port) {
+    port.postMessage(message);
+  }
+}
+
 window.onload = function() {
   $("#location-input").focus(geolocate);
+  $("#wake-button").click(setLocation);
   initialize();
 };
